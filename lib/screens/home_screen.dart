3088f2d2c3c9e4ba15ca98b8df699b3e12e477cf
IrakozeLoraine <mukezwa@gmail.com>
2025-07-19 21:27:54 +0200

    ft: add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:my_rights_mobile_app/core/theme/app_colors.dart';
+import 'package:ming_cute_icons/ming_cute_icons.dart';
+import 'package:my_rights_mobile_app/shared/widgets/custom_app_bar.dart';
+import 'package:my_rights_mobile_app/shared/widgets/custom_bottom_navbar.dart';
+import 'package:my_rights_mobile_app/shared/widgets/quick_access_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,20 +15,55 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: AppColors.primary,
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to MyRights!',
-          style: TextStyle(
-            fontSize: 24,
-            color: AppColors.textPrimary,
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  // Quick Access Section
+                  Text(
+                    'Quick Access',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).appBarTheme.foregroundColor
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  QuickAccessCard(
+                    icon: MingCuteIcons.mgc_book_line,
+                    title: 'Civic Education',
+                    description: 'Learn about your rights and responsibilities as a citizen.',
+                    onTap: () => {},
+                    // onTap: () => context.go(AppRouter.civicEducation),
+                  ),
+                  QuickAccessCard(
+                    icon: Icons.report_problem,
+                    title: 'Report an Issue',
+                    description: 'Report incidents of injustice or corruption.',
+                    onTap: () => {},
+                    // onTap: () => context.go(AppRouter.reportIssue),
+                  ),
+                  QuickAccessCard(
+                    icon: Icons.balance,
+                    title: 'Legal Aid',
+                    description: 'Find legal assistance and resources.',
+                    onTap: () => {},
+                    // onTap: () => context.go(AppRouter.legalAid),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
-      backgroundColor: AppColors.background,
+      bottomNavigationBar: const CustomBottomNavBar(),
     );
   }
 }
