f8d6bf2d6f56795f9929a62d8f5b470a29b77ebe
IrakozeLoraine <mukezwa@gmail.com>
2025-07-19 18:39:10 +0200

    ft: add custom app bar

---
+++ lib/shared/widgets/custom_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:my_rights_mobile_app/core/theme/app_colors.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      elevation: 0,
+      leadingWidth: 72,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 16.0),
+        child: Container(
+          width: 56,
+          height: 56,
+          decoration: BoxDecoration(
+            color: AppColors.primary,
+            borderRadius: BorderRadius.circular(100)
+          ),
+          child: Center(
+            child: Text(
+              'MR',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+      title: Text(
+        'MyRights',
+        style: Theme.of(context).appBarTheme.titleTextStyle?.copyWith(
+          fontWeight: FontWeight.w800
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 16);
+}
