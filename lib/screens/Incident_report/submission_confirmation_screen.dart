551866dc888b3acdf45658dc990da803e5271e46
Best-Verie <irabestverie2016@gmail.com>
2025-07-22 15:05:21 +0200

    confirm report screen

---
+++ lib/screens/Incident_report/submission_confirmation_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../shared/widgets/custom_button.dart';
+import '../../provider/incident_report_provider.dart';
+import 'view_report_screen.dart';
+
+class SubmissionConfirmationScreen extends ConsumerWidget {
+  const SubmissionConfirmationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final report = ref.watch(currentReportProvider);
+    if (report == null) {
+      return const Scaffold(
+        body: Center(child: Text('No submission found.')),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Submission Confirmation'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 32),
+            const Text(
+              'Your submission has been received',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Thank you for your submission. Your case has been successfully submitted and is now under review. You will receive updates via email and SMS.',
+            ),
+            const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Tracking Number:'),
+                Text(report.trackingNumber,
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Status:'),
+                Text(report.status.name,
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+            const SizedBox(height: 32),
+            CustomButton(
+              text: 'View Report',
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (_) => const ViewReportScreen(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
