900939b08eba6f6d8325bca47955a9167fd025f9
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2025-07-30 15:07:52 +0200

    ref: lawyer model updated

--- lib/models/lawyer.dart
+++ lib/models/lawyer.dart
@@ -1,19 +1,47 @@
+
 class Lawyer {
   final String id;
   final String name;
   final String organization;
+  final String? profileImageUrl;
+  final String? email;
+  final String? phoneNumber;
+  final String? address;
+  final List<String>? specializations;
+  final bool isAvailable;
+  final double? rating;
+  final int? experienceYears;
 
   Lawyer({
     required this.id,
     required this.name,
     required this.organization,
+    this.profileImageUrl,
+    this.email,
+    this.phoneNumber,
+    this.address,
+    this.specializations,
+    this.isAvailable = true,
+    this.rating,
+    this.experienceYears,
   });
 
+  // Factory constructor for JSON parsing
   factory Lawyer.fromJson(Map<String, dynamic> json) {
     return Lawyer(
-      id: json['id'],
-      name: json['name'] ?? '',
-      organization: json['organization'] ?? '',
+      id: json['id']?.toString() ?? '',
+      name: json['name']?.toString() ?? '',
+      organization: json['organization']?.toString() ?? '',
+      profileImageUrl: json['profile_image_url']?.toString(),
+      email: json['email']?.toString(),
+      phoneNumber: json['phone_number']?.toString(),
+      address: json['address']?.toString(),
+      specializations: json['specializations'] != null 
+          ? List<String>.from(json['specializations'])
+          : null,
+      isAvailable: json['is_available'] ?? true,
+      rating: json['rating']?.toDouble(),
+      experienceYears: json['experience_years']?.toInt(),
     );
   }
 }
