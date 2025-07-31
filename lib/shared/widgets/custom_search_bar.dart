3baa2b51f5a96485dc349dfae0963605c44737e7
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2025-07-31 14:21:36 +0200

    ref: update search bar to use stateful widget

--- lib/shared/widgets/custom_search_bar.dart
+++ lib/shared/widgets/custom_search_bar.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomSearchBar extends ConsumerWidget {
+class CustomSearchBar extends StatefulWidget {
   final ValueChanged<String> onChanged;
-  const CustomSearchBar({super.key, required this.onChanged});
+  final String query;
+  const CustomSearchBar({super.key, required this.onChanged, required this.query});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.query);
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomSearchBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.query != widget.query) {
+      _controller.text = widget.query;
+      _controller.selection = TextSelection.fromPosition(
+        TextPosition(offset: _controller.text.length),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(16),
       decoration: BoxDecoration(
@@ -14,14 +44,15 @@ class CustomSearchBar extends ConsumerWidget {
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withValues(alpha: 0.1),
+            color: Colors.black.withOpacity(0.1),
             blurRadius: 8,
             offset: const Offset(0, 2),
           ),
         ],
       ),
       child: TextField(
-        onChanged: onChanged,
+        controller: _controller,
+        onChanged: widget.onChanged,
         decoration: InputDecoration(
           hintText: 'Search for organization',
           hintStyle: Theme.of(context).inputDecorationTheme.hintStyle,
@@ -30,7 +61,7 @@ class CustomSearchBar extends ConsumerWidget {
             color: Theme.of(context).inputDecorationTheme.hintStyle?.color,
           ),
           border: InputBorder.none,
-          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         ),
       ),
     );
